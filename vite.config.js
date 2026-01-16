ec9ee60ab7124661b96035ae02ce42467d1e827a
Jaivardhan Bhola <jaivardhan.bhola@gmail.com>
2026-01-16 10:10:15 +0530

    Dockerized it

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.VITE_BACKEND_URL || 'http://localhost:3001',
         changeOrigin: true,
       },
     }
